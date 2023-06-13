69058750ccd51a5a637e98db2f8d5d499bd64caf
bao2k1 <mtpquangbao@gmail.com>
2023-06-13 18:49:02 +0700

    update entity

---
+++ src/main/java/cybersoft/example/cozastore/entity/ids/OrderDetailIds.java
@@ -0,0 +1,39 @@
+package cybersoft.example.cozastore.entity.ids;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class OrderDetailIds implements Serializable {
+
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "order_id")
+    private int orderId;
+
+    public OrderDetailIds(){}
+
+    public OrderDetailIds(int productId, int orderId){
+        this.productId = productId;
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+}
